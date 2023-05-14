eef9ca35e14224f7747ea48f79a5192068c1ac68
immuhc <sumanthchummisumanth@gmail.com>
2023-05-14 16:19:32 +0530

    duplicate elements project

---
+++ src/A.java
@@ -0,0 +1,21 @@
+// duplicate elements in array
+public class A {
+	public static void main(String[] args) {
+		int[] a = { 1, 2, 2, 3, 4, 5, 5 };
+		int[] temp = new int[a.length];
+
+		int j = 0;
+
+		for (int i = 0; i < a.length - 1; i++) {
+			if (a[i] != a[i + 1]) {
+				temp[j] = a[i];
+				j++;
+			}
+		}
+		temp[j] = a[a.length - 1];
+
+		for (int x = 0; x < temp.length; x++) {
+			System.out.println(temp[x]);
+		}
+	}
+}
